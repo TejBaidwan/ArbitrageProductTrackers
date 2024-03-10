c1ea9756d572cbefba00d26b6093aa01d5750e72
Evan Proulx <evanproulx7@gmail.com>
2024-03-10 15:18:35 -0400

    #40 -Added Scanner fragment that uses the code scanner api to scan barcodes and retrieve its information\n -Added Product class

---
+++ app/src/main/java/com/example/arbitragetracker/Scanner.java
@@ -0,0 +1,47 @@
+package com.example.arbitragetracker;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
+
+public class Scanner {
+    private Context context;
+
+    public Scanner(Context context) {
+        this.context = context;
+    }
+
+    public void startScan(@NonNull BarcodeScanListener listener) {
+        //sets settings for the scanner
+        //formats are what is being scanned(QR or barcodes)
+        GmsBarcodeScannerOptions options = new GmsBarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_AZTEC)
+                .build();
+
+        //runs the scanner
+        GmsBarcodeScanner scanner = GmsBarcodeScanning.getClient(context, options);
+
+        scanner.startScan()
+                .addOnSuccessListener(barcode -> {
+                    // Pass scanned barcode value to listener
+                    listener.onBarcodeScanned(barcode.getRawValue());
+                })
+                .addOnFailureListener(e -> {
+                    // Handle scan failure
+                    Log.e("BarcodeScanner", "Barcode scan failed: " + e.getMessage());
+                    listener.onScanFailed(e);
+                });
+    }
+
+    //This interface is needed to notify the caller when the scanning operation is complete
+    public interface BarcodeScanListener {
+        void onBarcodeScanned(@NonNull String barcodeValue);
+        void onScanFailed(@NonNull Exception e);
+    }
+}
---
+++ app/src/main/java/com/example/arbitragetracker/ScannerFragment.java
@@ -0,0 +1,133 @@
+package com.example.arbitragetracker;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ScannerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ScannerFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    TextView prodName,prodDesc,prodPrice;
+    ImageView productImg;
+    String barcodeValue = null;
+
+    Product productOnScreen;
+
+    public ScannerFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ScannerFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ScannerFragment newInstance(String param1, String param2) {
+        ScannerFragment fragment = new ScannerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_scanner, container, false);
+        Button scanBtn = view.findViewById(R.id.scanButton);
+        Button addBtn = view.findViewById(R.id.addButton);
+        TextView scannerOutput = view.findViewById(R.id.scannerOutput);
+        prodName = view.findViewById(R.id.productTitle);
+        prodDesc = view.findViewById(R.id.productDescription);
+        prodPrice = view.findViewById(R.id.productPrice);
+        productImg = view.findViewById(R.id.productImg);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleScan(scannerOutput);
+            }
+        });
+
+        return view;
+    }
+
+    private Task<Barcode> startScan() {
+        //Sets settings for the scanner
+        //Only scans QR and barcodes
+        GmsBarcodeScannerOptions options = new GmsBarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_AZTEC)
+                .build();
+
+        //runs the scanner
+        GmsBarcodeScanner scanner = GmsBarcodeScanning.getClient(getContext());
+        return scanner.startScan();
+    }
+
+    //This allows for the output to be returned
+    private void handleScan(TextView scannerOutput) {
+        startScan().addOnSuccessListener(new OnSuccessListener<Barcode>() {
+            @Override
+            public void onSuccess(Barcode barcode) {
+                // Handle successful scan
+                barcodeValue = barcode.getRawValue();
+                scannerOutput.setText("Code: " + barcodeValue);
+
+                //get the product data after scanning
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Handle scan failure
+                Log.d("Scanner Failed", e.getLocalizedMessage());
+                Toast.makeText(getContext(), "The scanner failed. Try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
