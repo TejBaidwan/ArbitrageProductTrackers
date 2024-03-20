68590125d09f318da8fb573a73eddaf64a866b3e
Tej Baidwan <w0806279@myscc.ca>
2024-03-19 23:36:56 -0400

    #8 Update Statistics Fragment to include nodes and bundled information passed via newInstance() Added the nodes found on the stats fragment into the newInstance() method for bundling and setting of information in the VP Host

--- app/src/main/java/com/example/arbitragetracker/statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/arbitragetracker/statistics/StatisticsFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.arbitragetracker.R;
 
@@ -21,10 +23,12 @@ public class StatisticsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mParam1;
+    private int mParam2;
+    private double mParam3;
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -36,14 +40,16 @@ public class StatisticsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
+     * @param param3 Parameter 3
      * @return A new instance of fragment StatisticsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StatisticsFragment newInstance(String param1, String param2) {
+    public static StatisticsFragment newInstance(int param1, int param2, double param3) {
         StatisticsFragment fragment = new StatisticsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, param1);
+        args.putInt(ARG_PARAM2, param2);
+        args.putDouble(ARG_PARAM3, param3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +58,9 @@ public class StatisticsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+            mParam2 = getArguments().getInt(ARG_PARAM2);
+            mParam3 = getArguments().getDouble(ARG_PARAM3);
         }
     }
 
@@ -61,6 +68,18 @@ public class StatisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        TextView statsTitle = view.findViewById(R.id.statTitle);
+        ImageView statsImage = view.findViewById(R.id.statsImage);
+        TextView statsValue = view.findViewById(R.id.statsValue);
+
+        if (mParam1 != 0 && mParam2 != 0 && mParam3 != 0) {
+            statsTitle.setText(mParam1);
+            statsImage.setImageResource(mParam2);
+            statsValue.setText((int) mParam3);
+        }
+
+        return view;
     }
 }
